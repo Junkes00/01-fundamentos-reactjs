d2e68eb124c2d0cbe9e6e8b3ca13ca612a225378
Nicolas Junkes <nicolas.junkes00@gmail.com>
2023-02-27 00:34:05 -0300

    Altera a tag img para o component Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,7 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/Junkes00.png" />
+        <Avatar src="https://github.com/Junkes00.png" />
 
         <strong>Nicolas Junkes</strong>
         <span>Web Developer</span>
