73e551b87ec87b16b3f564e7fd326dd11c5b0a32
Nicolas Junkes <nicolas.junkes00@gmail.com>
2023-02-27 00:28:01 -0300

    Cria um objeto estático para testes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,51 @@ import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Junkes00.png",
+      name: "Nicolas Junkes",
+      role: "Software Engineer @Havan Labs",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-02-25 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-02-23 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <>
